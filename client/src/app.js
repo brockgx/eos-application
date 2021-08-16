36580947ea9f59a6bf185410c92804f90429f6d9
CompSciAlex <88032690+CompSciAlex@users.noreply.github.com>
2021-08-16 19:07:58 +1000

    This is to test

--- client/src/app.js
+++ client/src/app.js
@@ -7,7 +7,7 @@ import { Home } from './pages/home';
 import { Concept } from './pages/concept-test';
 
 //All component imports here
-
+//<><><><><><><><This is a test addition to the file to confirm this is working properly
 //This is the main application component for React
 //it houses the view, which is a single navbar component
 //and the various other pages, which are routed using React-Router
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Hello dude</h1>
+        <h1>Hello dude this is a test</h1>
         <Route path="/" exact component={Home} />
 
         {/*The route used for the concept of the dataflow*/}
