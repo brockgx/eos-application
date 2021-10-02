711e629978661efd045206c4773596ecf20710ba
MYRON TSALDARIS <100909217@student.swin.edu.au>
2021-10-02 12:37:55 +1000

    created route to get machines and metrics

--- client/src/pages/query.js
+++ client/src/pages/query.js
@@ -3,6 +3,8 @@ import {useState, useEffect} from 'react'
 import { DataGrid } from "@material-ui/data-grid";
 import { Columns } from '../components/query/ColumnData';
 import styled from 'styled-components';
+import { Modal, TextField, FormControl, Select, MenuItem, InputLabel } from '@material-ui/core';
+
 
 // import '../styles/query.css';
 
@@ -34,74 +36,96 @@ const TopText = styled.span`
   padding-bottom: 10px;
 `;
 
+const Text = styled.span`
+  font-weight: 400;
+  font-size: 22px;
+  padding-bottom: 10px;
+`;
+
 const Bottom = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  padding: 0px 40px;
-`;
-
-const Left = styled.div`
-  flex: 1;  
-  display: flex;
-  padding: 5px;
-  flex-direction: column;
-  height: 75vh;
-`;
-const Form = styled.div`
   border-radius: 5px;
-  padding: 30px;
+  justify-content: space-between;
   background-color: #ffffff;
   -webkit-box-shadow: 0px 0px 15px -7px rgba(0, 0, 0, 0.8);
   box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+`;
+
+const FormWrapper = styled.div`
+  padding: 30px;
+`;
+
+const Form = styled.form`
+  padding: 30px;
   height: 100%;
 `;
 
-const Right = styled.div`
-  flex: 1;  
-  display: flex;
-  flex-direction: column;
-  padding: 5px;
-
-  height: 75vh;
-  background-colour: white;
+const FormItem = styled.div`
 `;
-
-// const BottomText = styled.span`
-//   font-weight: 400;
-//   font-size: 38px;
-//   padding-bottom: 20px;
-// `;
 
 const QueryResult = styled.div`
-  border-radius: 5px;
   padding: 20px;
-  background-color: #ffffff;
   height: 700px;
-  -webkit-box-shadow: 0px 0px 15px -7px rgba(0, 0, 0, 0.8);
-  box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
 `;
 
+const defaultValues = {
+  name: ""
+};
 
 
 const Query = (props) => {
-    const [messages, setMessages] = useState({description: "default desc", content: []})
-  
+    const [query, setQuery] = useState(defaultValues)
+    const [machines, setMachines] = useState({description: "default desc", content: []})
+    const [machineMetrics, setMachineMetrics] = useState({description: "default desc", content: []})
+
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setQuery({ ...query, [name]: value,});
+      fetch(`/query/clientmachinemetrics?name=${query.name}`, {
+        method: 'GET',
+        body: query.name
+      }).then(() => {
+        window.location.reload();
+      })
+    };
+
+    // Get all machines from db to display in drop down
     useEffect(() => {
-      const getMessages = async () => {
-        const messagesFromServer = await fetchMessages()
-        setMessages(messagesFromServer)
+      const getMachines = async () => {
+        const machinesFromServer = await fetchMachines()
+        setMachines(machinesFromServer)
       }
   
-      getMessages()
+      getMachines()
+    }, [])
+
+    const fetchMachines = async () => {
+      const resp = await fetch('/query/clientmachines')
+      const data = await resp.json()
+      if(resp.ok) {
+        return data;
+      } else {
+        throw Error(`Request rejected with status ${resp.status}`);
+      }
+    }
+
+    // Get sys and app metrics from db based on machine name
+    useEffect(() => {
+      const getMetrics = async () => {
+        const data = await fetchMetrics()
+        setMachineMetrics(data)
+      }
+  
+      getMetrics()
     }, [])
   
     // Fetch device data from DB
-    const fetchMessages = async () => {
-      const resp = await fetch('/test/getmetrics')
+    const fetchMetrics = async () => {
+      const resp = await fetch(`/query/clientmachinemetrics?name=${query.name}`)
       const data = await resp.json()
       if(resp.ok) {
-        //console.log(data.content[4].message)
+        console.log(data.content)
         return data;
       } else {
         throw Error(`Request rejected with status ${resp.status}`);
@@ -115,15 +139,30 @@ const Query = (props) => {
             <TopText>Query Database</TopText>
           </Top>
           <Bottom>
-            <Left>
-              
               <Form>
-                [TODO - Query Form]
+              <Text>Choose a machine to query</Text>
+                <FormItem>
+                  <FormControl style={{width: "100%"}}>
+                    <InputLabel>Machine Name</InputLabel>
+                    <Select
+                      name="name"
+                      value={query.name}
+                      onChange={handleInputChange}
+                    >
+                      <MenuItem key="all" value="all">
+                        All Machines
+                      </MenuItem>
+                      {machines.content.map((machine) => (
+                        <MenuItem key= {machine.name} value={machine.name}>
+                          {machine.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </FormItem>
               </Form>
-            </Left>
-            <Right>
               <QueryResult>
-                <DataGrid
+                {/* <DataGrid
                   className="deviceList"
                   rows={messages.content}
                   disableColumnMenu
@@ -131,11 +170,9 @@ const Query = (props) => {
                   pageSize={15}
                   rowsPerPageOptions={[15, 25, 50]}
                   columns={Columns}
-                />
+                /> */}
               </QueryResult>
-            </Right>
           </Bottom>
-          
         </Wrapper>
 
       </Container>
