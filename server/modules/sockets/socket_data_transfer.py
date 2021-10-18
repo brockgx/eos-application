d4b48600dc5e3394f44e3f8ec3f24f3b5e276b77
Brock Giller <Brock.Giller@gmail.com>
2021-10-18 23:32:04 +1100

    Finalised structure for myron

--- server/modules/sockets/socket_data_transfer.py
+++ server/modules/sockets/socket_data_transfer.py
@@ -1,9 +1,5 @@
-#import third party libraries
-import pickle
-import time
-
 #import personal libraries
-#from ..security.AESEncryption import serialize
+# from ..security.AESEncryption import do_encrypt, do_decrypt
 
 #Constants
 HEADERSIZE = 10
@@ -14,57 +10,50 @@ RECVSIZE = 16
 #Function: send data
 def sendSocketData(socketConn, message):
   #The full message to send with the buffer
-  #full_msg = bytes(f'{len(str(serialized_msg)):<{HEADERSIZE}}' + str(serialized_msg)[2:][:-1], "utf-8")
-  #encrypted_test = serialize(message)
-  #print(encrypted_test)
-  fullMsg = bytes(f'{len(message):<{HEADERSIZE}}' + message, "utf-8")
-  #length = bytes(f'{len(str(encrypted_test)):<{HEADERSIZE}}')
-  #full_msg = length + encrypted_test
-  #print(full_msg)
-  
+  encryptedMsg = do_encrypt(message)
+  encryptedtestLen = str(len(str(encryptedMsg)))
 
   #Send the message if available
   try:
-    socketConn.sendall(fullMsg)
-  except:
+    socketConn.sendall(encryptedtestLen.encode())
+    socketConn.sendall(b'\0')
+    socketConn.sendall(encryptedMsg)
+  except Exception as error:
     print("Failure message")
+    print(str(error))
 
 #Function: receive data
 def receiveSocketData(socketConn):
   #function variables
-  #receieved_msg = "EMPTY"
   receiveMsg = True
   newMsg = True
-  lengthMsg = True
+  lengthMsg = 0
   completeMsg = b''
 
   while receiveMsg:
     try:
       msg = socketConn.recv(RECVSIZE)
+
+      if newMsg == False:
+        completeMsg += msg
     except:
       receiveMsg = False
 
     if msg != b'':
       if newMsg:
-        lengthMsg = int(msg[:HEADERSIZE])
+        splitMsg = msg.split(b'\0')
+        lengthMsg = int(splitMsg[0].decode())
         newMsg = False
-        
-      completeMsg += msg
-      
-      if len(completeMsg)-HEADERSIZE == lengthMsg:
+        completeMsg+=splitMsg[1]
+
+      if len(str(completeMsg)) == lengthMsg:
         #Do something with the data - print example
-        receievedMsg = completeMsg[HEADERSIZE:].decode("utf-8")
-
-        #Decrypt here
-        #my_msg = receieved_msg[2:][:-1]
-        #print(my_msg)
-        #byte_msg = my_msg.encode()
-        #print(byte_msg)
-        #deserialized = pickle.loads(byte_msg)
-
-
+        print(completeMsg)
+        print("\n")
+        receievedMsg = do_decrypt(completeMsg)
+        receievedMsg = receievedMsg.decode()
         newMsg = True
         completeMsg = ''
         receiveMsg = False
-        
+          
   return receievedMsg
