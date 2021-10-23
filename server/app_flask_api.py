61e3d9503e700022b7e534dbfacd4e1363cdc0cb
Brock Giller <Brock.Giller@gmail.com>
2021-10-23 18:18:38 +1100

    added final config changes for the server

--- server/app_flask_api.py
+++ server/app_flask_api.py
@@ -1,8 +1,10 @@
 #Import any downloaded/third party modules and packages
 from flask import Flask, jsonify
+from sys import exit
 
 #Import any custom made modules
 from modules.utilities.server_config import get_config_details
+from modules.utilities.server_logging import server_logger
 from modules.routes.test_routes import test_routes
 from modules.routes.command_routes import command_routes
 from modules.routes.dashboard_routes import dashboard_routes
@@ -10,47 +12,51 @@ from modules.routes.query_routes import query_routes
 from modules.routes.metric_routes import metric_routes
 from modules.database.database_tables import db
 
-#Setup any constants need to be used
-HOST = "127.0.0.1"
-PORT = 5000
-HTTPS_ENABLED = False
-CONTEXT = ('cert file', 'key file')
-
 #Need to setup config file for the API
-cfg_details = get_config_details()
+config_dets = get_config_details()
 
 #Instantiate the flask application
 app = Flask(__name__)
 
-#Instantiate the DB (SQLLite for testing)
-app.config["SQLALCHEMY_DATABASE_URI"] = cfg_details["DATABASE-URI"] + cfg_details["DATABASE"]
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
-db.app = app
-db.init_app(app)
-db.create_all()
+if config_dets != False:
+  server_logger.info("Starting API server...")
+  #Instantiate the DB (SQLLite for testing)
+  try:
+    DB_LOCATION = config_dets["DATABASE-DETAILS"]["DATABASE-URI"] + config_dets["DATABASE-DETAILS"]["DATABASE-NAME"]
+    if config_dets["DATABASE-DETAILS"]["DATABASE-TYPE"] == "mysql":
+      DB_PATH = "mysql+pymysql://" + config_dets["DATABASE-DETAILS"]["USERNAME"] + config_dets["DATABASE-DETAILS"]["PASSWORD"] + "@" + DB_LOCATION
+    else:
+      DB_PATH = "sqlite:///" + DB_LOCATION
+    app.config["SQLALCHEMY_DATABASE_URI"] = DB_PATH
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+  except Exception as err_msg:
+    server_logger.critical("Database configuration error - {}".format(str(err_msg)))
+    exit()
 
-##CONTENT##
-@app.route('/', methods=['GET'])
-def home_route():
-  return jsonify(
-    title="Main route",
-    user="none"
-  )
+  ##CONTENT##
+  @app.route('/', methods=['GET'])
+  def home_route():
+    return jsonify(
+      title="Main route",
+      user="none"
+    )
 
-#Implement route modules
-app.register_blueprint(test_routes, url_prefix='/test')
-app.register_blueprint(command_routes, url_prefix='/commands')
-app.register_blueprint(dashboard_routes, url_prefix='/dash')
-app.register_blueprint(query_routes, url_prefix='/query')
-app.register_blueprint(metric_routes, url_prefix='/metrics')
+  #Implement route modules
+  app.register_blueprint(test_routes, url_prefix='/test')
+  app.register_blueprint(command_routes, url_prefix='/commands')
+  app.register_blueprint(dashboard_routes, url_prefix='/dash')
+  app.register_blueprint(query_routes, url_prefix='/query')
+  app.register_blueprint(metric_routes, url_prefix='/metrics')
 
-#Function for running the app
-def start_flask_api():
+  #Run the Flask app server
   if __name__ == '__main__':
-    if HTTPS_ENABLED:
+    if config_dets["HTTPS-DETAILS"]["HTTPS-ENABLED"]:
       print("HTTPS ENABLED")
       #app.run(host=HOST,port=PORT,debug=True,ssl_context=CONTEXT)
     else:
-      app.run(host=HOST,port=PORT,debug=True)
-
-start_flask_api()
+      app.run(host=config_dets["SERVER-DETAILS"]["SERVER-ADDRESS"],port=config_dets["SERVER-DETAILS"]["SERVER-PORT"],debug=True)
+else:
+  server_logger.critical("Failed to start the API server, issue with config file")
--- server/modules/utilities/server_config.py
+++ server/modules/utilities/server_config.py
@@ -5,18 +5,71 @@ import yaml
 from .server_logging import server_logger
 #import yaml, os, platform
 
-#Get the directory of the configuration file
-CONFIG_DIR = path.abspath(path.join(__file__,"../../../"))
-if system() == "Windows":
-  CONFIG_PATH = CONFIG_DIR + "\\server_config.yml"
-else:
-  CONFIG_PATH = CONFIG_DIR + "/server_config.yml"
+def read_config_file():
+  #Get the directory of the configuration file
+  CONFIG_DIR = path.abspath(path.join(__file__,"../../../"))
+  if system() == "Windows":
+    CONFIG_PATH = CONFIG_DIR + "\\server_config.yml"
+  else:
+    CONFIG_PATH = CONFIG_DIR + "/server_config.yml"
 
-with open(CONFIG_PATH, "r") as ymlfile:
-  cfg = yaml.safe_load(ymlfile)
+  try:
+    with open(CONFIG_PATH, "r") as ymlfile:
+      return yaml.safe_load(ymlfile)
+  except FileNotFoundError as err_msg:
+    server_logger.critical("Config file missing - {}.".format(str(err_msg)))
+    return False
+  except Exception as err_msg:
+    server_logger.critical("Could not load conifg file details - {}.".format(str(err_msg)))
+    return False
 
 #Config file is loading successfully, now need to validate entries in file
 #Then return all the entries once valid
+def validate_sections(config_dets, sections):
+  for valid_sec in sections:
+    if valid_sec not in config_dets:
+      server_logger.error("Missing section {} in config file.".format(valid_sec))
+      return False
+
+  return True
+
+def validate_section_details(config_dets, items, types):
+  for detail in config_dets:
+    #print(types[items.index(item)] + " : " + str(type(config_dets[item])) + " : " + item)
+    if detail in items:
+      if isinstance(config_dets[detail],types[items.index(detail)]):
+        result = True
+      else:
+        server_logger.error("{} must be of type {}".format(str(config_dets[detail]), str(types[items.index(detail)])))
+        return False
+    else:
+      server_logger.error("Missing detail {} in config file".format(str(detail)))
+      return False
+  return True
+    
+    
+def validate_config_details():
+  #Read config file
+  cfg = read_config_file()
+  if cfg != False:
+    #Validate sections
+    all_items = {
+      "SERVER-DETAILS": {"names": ["SERVER-ADDRESS","SERVER-PORT","PORT-ENABLED"], "types": [str,int,bool]},
+      "HTTPS-DETAILS": {"names": ["HTTPS-ENABLED","CERTIFICATE-FILE","KEY_FILE"], "types": [bool,str,str]},
+      "DATABASE-DETAILS": {"names": ["USERNAME","PASSWORD","DATABASE-TYPE","DATABASE-NAME","DATABASE-URI"], "types": [str,str,str,str,str]} 
+    }
+    sections_valid = validate_sections(cfg, all_items)
+    #Validate values
+    if sections_valid:
+      for section in all_items:
+        valid_items = validate_section_details(cfg[section], all_items[section]["names"], all_items[section]["types"])
+        if not valid_items:
+          return False
+      return cfg
+    else:
+      return False
+  else:
+    return False
 
 def get_config_details():
-  return cfg["DATABASE-DETAILS"]
+  return validate_config_details()
